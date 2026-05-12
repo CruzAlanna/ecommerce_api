504a433b06bb618255d508569f441937d31c99eb
CruzAlanna <lannikcru@gmail.com>
2026-05-12 18:48:03 +1000

    Seeded Data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,21 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+
+product = Product.new({name: "WNYX Mug", price: 2, description: "Get your morning news once you wake up with a cup of joe from... well Joe. He made it with his homemade duct tape"})
+product.save
+
+product = Product.new({name: "Hitchhiker's Guide to the Galaxy", price: 42, description: "In many of the more relaxed civilizations on the Outer Eastern Rim of the Galaxy, the Hitch-Hiker's Guide has already supplanted the great Encyclopaedia Galactica as the standard repository of all knowledge and wisdom, for though it has many omissions and contains much that is apocryphal, or at least wildly inaccurate, it scores over the older, more pedestrian work in two important respects. First, it is slightly cheaper; and secondly it has the words DON'T PANIC inscribed in large friendly letters on its cover."})
+product.save
+
+product = Product.new({name: "Lightsaber", price: 270, description: "Part laser, part samurai sword, all awesome. The lightsaber is an elegant weapon for a more civilized age, not nearly as clumsy as a blaster"})
+product.save
+
+product = Product.new({name: "Space Cowboy Laser Gun", price: 170, description: "This weapon has no other description than, Shiney!"})
+product.save
+
+product = Product.new({name: "DnD Dice set", price: 57, description: "Take down mighty dragons with this timeless set of 20 sided wonders"})
+product.save
+
+product = Product.new({name: "Sonic Screwdriver", price: 9, description: "The Doctor's sciencey magic wand to get out of tight spots. Note: does not work on wood"})
+product.save
+
+product = Product.new({name: "Yoda sleeping bag", price: 40, description: "For real"})
+product.save

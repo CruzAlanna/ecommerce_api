2f510a7a52f9944195d2f86a1674bbefaff6356f
CruzAlanna <lannikcru@gmail.com>
2026-05-12 18:41:51 +1000

    Added Product model

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 0) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_12_083757) do
+  create_table "products", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "description"
+    t.string "name"
+    t.integer "price"
+    t.datetime "updated_at", null: false
+  end
 end
